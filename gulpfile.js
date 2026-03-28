88c1b9057c1410e7da53ec09c97f442b154b7e51
KhonThanh <tangkhonthanh@gmail.com>
2026-03-28 11:36:09 +0700

    update
    html
    css
    js

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,25 @@ const gulp = require('gulp');
 const concat = require('gulp-concat');
 const cleanCSS = require('gulp-clean-css');
 
-gulp.task('pack-css', function () {
+// 1. Task nén CSS (đặt tên là pack-css)
+gulp.task('pack-css', function () {    
     return gulp.src([
-        'css/font.css',        // Font thường ưu tiên đầu
-        'css/base.css',        // Base reset
-        'css/utility.css',     // Tiện ích
-        'css/custom.css',      // Code của bồ
-        'css/responsive.css'   // Responsive phải ở cuối cùng để đè các class trên
+        'src/css/font.css',
+        'src/css/base.css',
+        'src/css/utility.css',
+        'src/css/custom.css',
+        'src/css/responsive.css'
     ])
-        .pipe(concat('style.min.css'))
-        .pipe(cleanCSS())
-        .pipe(gulp.dest('dist/css'));
+    .pipe(concat('style.min.css')) 
+    .pipe(cleanCSS())              
+    .pipe(gulp.dest('dist/css'));  
 });
+
+// 2. Task theo dõi sự thay đổi (đặt tên là watch)
+gulp.task('watch', function() {
+    // Nó sẽ canh chừng tất cả file .css trong thư mục src/css
+    gulp.watch('src/css/*.css', gulp.series('pack-css')); 
+});
+
+// 3. Task mặc định (Chỉ cần gõ npx gulp là chạy cả 2)
+gulp.task('default', gulp.series('pack-css', 'watch'));
